5078405fe69eb7630773d5606399d078274013f2
Junaid <jk1240651@gmail.com>
2024-04-07 22:54:51 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const _ = require("lodash");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
