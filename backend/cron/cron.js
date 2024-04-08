9842b0fc984ce36a01718b87bc1ad4f4422361dc
Junaid Khan <jk1240651@gmail.com>
2024-04-08 11:18:23 +0530

    Update cron.js

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,10 +1,10 @@
 const corn = require("cron");
 const http = require("http");
-const URL = "https://threads-clone-9if3.onrender.com";
+const URL = "https://canvas-whiteboard-and-document-editor.onrender.com/api/users/logout";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new cron.CronJob("*/4 * * * *", function () {
   https
-    .get(URL, (res) => {
+    .post(URL, (res) => {
       if (res.statusCode === 200) {
         console.log("GET request sent successfully");
       } else {
